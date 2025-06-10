7c993b54496d50528ded18f347dc5a9eae6d15a1
AaryanBasnet <basnetaryan1011@gmail.com>
2025-06-11 00:47:44 +0545

    feat: add CORS support to the application

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./database/db");
-const authRoutes = require("./route/authRoutes")
+const authRoutes = require("./route/authRoutes");
 
 const app = express();
 const PORT = process.env.PORT;
+
+const corsOption = {
+  origin: true,
+  credential: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOption));
 
 app.use(express.json());
 
 connectDB();
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`server runnning on port ${PORT}`);
