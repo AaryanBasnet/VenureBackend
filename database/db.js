e204a4675c9a4e9c4809427e1c5cc7c959c89602
AaryanBasnet <basnetaryan1011@gmail.com>
2025-07-08 19:34:17 +0545

    Added Dockerfile for Deployment

--- database/db.js
+++ database/db.js
@@ -5,6 +5,7 @@ const CONNECTION_STRING = process.env.DB_URL;
 const connectDB = async() => {
     try {
         await mongoose.connect(CONNECTION_STRING);
+        console.log("MongoDb Connected")
 
 
     } catch (err) {
